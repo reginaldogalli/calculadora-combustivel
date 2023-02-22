fde60f34207287ef23a8d7aa0c93f68be3ac041f
Reginaldo G Santos <85833964+reginaldogalli@users.noreply.github.com>
2023-02-22 13:10:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const calcular = document.getElementById('calcular');
+
+function calc () {
+    const valor = document.getElementById('valor').value.replace(",", ".");
+    const km = document.getElementById('km').value.replace(",", ".");
+    const resultado = document.getElementById('resultado');
+
+    if (valor !== '' && km !== ''){
+        const valorCalculado = (valor/km).toFixed(2);
+        resultado.textContent = 'O valor de cada Km rodado é de: R$' + valorCalculado;
+    } else {
+        resultado.textContent ='Preencha todos os campos!';
+    }
+}
+
+calcular.addEventListener('click', calc);
